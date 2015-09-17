0e233a705317c5242e07092982c5edf8eca459d9
Damien Dormal <dormal.damien@gmail.com>
2015-09-17 21:29:14 +0800

    works, simple and not really tested yet

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Hue state
-Small deamon to manage state of your hue ligths after power off.
+Small deamon to manage state of your hue lights after power off.
 
 ## Setup
 
@@ -9,18 +9,20 @@ npm install
 ```
 
 ### Bridge ip
-Then you need to know your bridge ip address. You can visit [meethue|http://www.meethue.com/api/nupnp]
+You need to know your bridge ip address. You can visit [meethue site](http://www.meethue.com/api/nupnp) to help you out.
 
 ### Create username
+run the following in a terminal (you need curl)
 ```
 curl -H "Content-Type: application/json" -X POST -d '{"devicetype":"my_hue_app#android yourname"}' http://BRIDGE_IP/api
 ```
-it will tell you to push the button, do it, then call the same. It should return a new username.
+it will tell you to push the button, do it, then call that again. It should return a new username.
 
 ## Run
 ```
 node main.js --host=BRIDGE_IP --username=USERNAME
 ```
-
-## TODO
-Improved logging
+or even better using [forever](https://github.com/foreverjs/forever)
+```
+forever start main.js --host=BRIDGE_IP --username=USERNAME
+```
--- main.js
+++ main.js
@@ -1,26 +1,39 @@
 #!/usr/bin/env node
 "use strict";
 const _ = require('lodash');
+const util = require('util');
 const rest = require('restler');
 const argv = require('optimist')
-    .default('interval', 1000)
+    .default('interval', 2000)
     .argv;
 
 let baseUrl = `http://${argv.host}/api/${argv.username}`
-let currentStates = {};
+let prevStates = {};
 
+function resetLamp(lampName, state) {
+  util.log(`going to reset lamp ${lampName}`);
+  // only pick the brithness and hue if present in state
+  let updatePayload = _.pick(state, ['bri', 'hue']);
+  util.log(updatePayload);
+  rest.putJson(`${baseUrl}/lights/${lampName}/state`, updatePayload).on('complete', (data, response) => {
+    // TODO handle failure
+  });
+};
 
-rest.get(baseUrl + '/lights').on('complete', result => {
-  if (result instanceof Error) {
-    console.log('Error:', result.message);
-  } else {
-    let newStates = _.mapValues(result, v => v.state);
-    //let newStates = _.map(result, (v, k) => { return { name : k, state: v.state } });
-    console.log(newStates);
-  }
-});
-
-// TODO avoid overloading the bridge, so thinking of something different then interval ( setTimeout on done )
-// setInterval(function() {
-//   api.fullState().then(displayResult).done();
-// }, argv.interval);
+setInterval(() => {
+  rest.get(baseUrl + '/lights').on('complete', result => {
+    if (result instanceof Error) {
+      util.log('Error:', result.message);
+    } else {
+      let states = _.mapValues(result, v => v.state);
+      // detect a newly reachable lamp
+      _.each(states, (state, lampName) => {
+        if (state.reachable && prevStates[lampName] && !prevStates[lampName].reachable) {
+          resetLamp(lampName, prevStates[lampName]);
+        }
+      });
+      // replace prev states with current
+      prevStates = states;
+    }
+  });
+}, argv.interval);
